7c8ff8cc47f8cb44d76e84536f9c535fff33beb4
Kumar Navneet <Navneet.Kumar@bcg.com>
2023-11-30 11:14:32 +0530

    Updates

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import eco2ai
 import time
-
-
-def run_experiment():
-    # Your code here
-    for i in range(1000):
-        print(i)
-        time.sleep(1)
-
+from models.sign_language_mnist import train_model
 
 if __name__ == "__main__":
     tracker = eco2ai.Tracker(
         project_name="ECO2AI Usage Example",
-        experiment_description="training_sign_language_mnist_model")
+        experiment_description="training_sign_language_mnist_model",
+        file_name="output/emissions_sign_mnist_basic.csv",
+    )
     tracker.start()
-    run_experiment()
+    print("Tracker started")
+    print("Model training started")
+    train_model()
     tracker.stop()
+    print("Tracker stopped")
